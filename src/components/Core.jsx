6028aa7b80bded8cb36dfe6e539b97829782baa1
sarthak2004 <sarthakishu2004@gmail.com>
2024-10-07 23:53:54 +0530

    about changes

--- src/components/Core.jsx
+++ src/components/Core.jsx
@@ -8,6 +8,7 @@ const Core = () => {
         <h1 className="text-[5vw] md:text-[100px] font-bold mb-4">Core Team '24</h1>
       </div>
 
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 px-4 md:px-10 mt-8">
         {coreTeamMembers.map((member, index) => (
           <a
@@ -15,12 +16,12 @@ const Core = () => {
             href={member.linkedinUrl} // Use the LinkedIn URL from your data
             target="_blank" // Open in new tab
             rel="noopener noreferrer" // Security measure
-            className="flex flex-col items-center bg-[#292f45] p-6 rounded-lg transition-transform duration-200 hover:scale-105"
+            className="flex flex-col items-center bg-[#292f45] p-6 rounded-lg transition-transform duration-200 hover:scale-105 aspect-w-1 aspect-h-1"
           >
             <img
               src={member.imageUrl}
               alt={`${member.name} profile`}
-              className="rounded-3xl mb-4 w-24 h-24 md:w-32 md:h-32 object-cover" // Set fixed width/height and object-cover
+              className="h-52 w-full object-cover object-end rounded-lg" // Set fixed width/height and object-cover
             />
             <h2 className="text-xl md:text-2xl font-semibold text-center">{member.name}</h2>
             <p className="text-gray-400 text-center">{member.designation}</p>
