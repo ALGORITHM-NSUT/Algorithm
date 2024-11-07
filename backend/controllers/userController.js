62d2d6d54ac65715a5851a1242ea9ae648de2ded
sarthak2004 <sarthakishu2004@gmail.com>
2024-11-08 04:07:55 +0530

    logout domain fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -176,7 +176,8 @@ export const logout = async (req, res, next) => {
             .clearCookie("token", {
                 httpOnly: true,
                 secure: true,
-                sameSite: "none"
+                sameSite: "none",
+                domain: ".vercel.app"
             })
             .json({
                 success: true,
