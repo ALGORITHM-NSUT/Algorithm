d3e34db62ff9f7aee4fa250eee280780facf0a87
Vatsal Veerwal <vatsalveerwal@gmail.com>
2024-12-26 19:10:38 +0530

    api call

--- frontend/src/auth/LeaderboardProvider.jsx
+++ frontend/src/auth/LeaderboardProvider.jsx
@@ -5,12 +5,15 @@ const LeaderboardContext = createContext();
 const LeaderboardProvider = ({ children }) => {
 	const [leaderboard, setLeaderboard] = useState([]);
 	const fetchLeaderboard = useCallback(async () => {
-		// Dummy data for now
-		setLeaderboard([
-			{ name: 'Alice', handle: 'alice123', score: 1200, leetcode: 2345 , codeForces: 1764 , codeChef: 9872 },
-			{ name: 'Bob', handle: 'bob99', score: 1100, leetcode: 9038, codeForces: 7422, codeChef: 9284 },
-			{ name: 'Charlie', handle: 'charlie', score: 1050, leetcode: 9874, codeForces:2736 , codeChef:5392 },
-		]);
+		try {
+			const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/leaderboard/show`, {
+				method: 'GET',
+			});
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.error('Error fetching user:', error);
+		}
 	});
 	useEffect(() => {
 		fetchLeaderboard();
