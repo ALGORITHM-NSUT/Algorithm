62975863f7320afbec4cae3f3bf5995ff7c3b920
sarthak2004 <sarthakishu2004@gmail.com>
2025-01-17 00:09:04 +0530

    changed width for LeaderBoardList member

--- frontend/src/components/Leaderboard/LeaderboardListMember.jsx
+++ frontend/src/components/Leaderboard/LeaderboardListMember.jsx
@@ -38,26 +38,27 @@ const LeaderboardListMember = ({
 
   const isTopRank = rank <= 3;
 
-  const style = isTopRank ? rankStyles[rank - 1] : {}; // for the border gradients (top 3 ranks)
+  const style = isTopRank ? rankStyles[rank - 1] : {};
 
   return (
     <div
-      className={`
+      className={`w-full cursor-pointer
         ${isUser && !isTopRank ? 'shadow-[0_0px_8px_rgba(150,0,255,1)]' : ''} 
-        ${isTopRank ? topRankOutsideGlow[rank - 1] : ''}
-         overflow-hidden rounded-md my-1.5`}
+        ${isTopRank ? topRankOutsideGlow[rank - 1] : ''} 
+        overflow-hidden rounded-md my-1.5`}
     >
       <li
         onClick={() => toggleModal(member)}
         style={style}
-        className={`${isTopRank ? topRankInsideGlow[rank - 1] : ''}
+        className={`w-full 
+          ${isTopRank ? topRankInsideGlow[rank - 1] : ''} 
           ${
             isUser
               ? 'bg-gradient-to-r from-gray-700/60 via-slate-500/60 to-gray-700/60'
               : 'dark:bg-gray-700/60'
-          }
-     
-          overflow-hidden py-5 px-4 border-solid  rounded-sm dark:hover:bg-gray-600 my-[0px] duration-75 backdrop-blur-md`}
+          } 
+          overflow-hidden py-5 px-4 border-solid rounded-sm 
+          dark:hover:bg-gray-600 my-[0px] duration-75 backdrop-blur-md`}
       >
         <div className="flex items-center justify-between w-full">
           <span className="text-md font-bold text-gray-700 dark:text-gray-300 w-10">
@@ -67,15 +68,11 @@ const LeaderboardListMember = ({
             <p className="text-md font-medium text-gray-900 dark:text-gray-100">
               {member.name}
             </p>
-            {/* <p className="text-sm text-gray-500 dark:text-gray-400">
-              {member.codeforcesHandle}
-              </p> */}
           </div>
           {isUser && <p className="ml-2 font-semibold">(You)</p>}
           <div className="flex-grow">
             <p className="text-lg font-semibold text-blue-600 dark:text-blue-400 justify-self-end">
               {(Math.round(member.score * 10000) / 100).toFixed(2)}
-              {/* this makes sure that the number is displayed upto 2 decimal places */}
             </p>
           </div>
         </div>
@@ -85,4 +82,3 @@ const LeaderboardListMember = ({
 };
 
 export default LeaderboardListMember;
-// dark:hover:bg-gray-600 my-[0px] transition-colors duration-75
